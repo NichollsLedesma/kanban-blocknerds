3fccc80d9d3ed0a219b3a62eb5b334173beb7c97
Nico Ledesma <nicoledesma36@gmail.com>
2021-05-13 09:47:19 -0300

    first commit

---
+++ src-app/common/config/main.php
@@ -0,0 +1,26 @@
+<?php
+return [
+    'aliases' => [
+        '@bower' => '@vendor/bower-asset',
+        '@npm'   => '@vendor/npm-asset',
+    ],
+    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'components' => [
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'port' => '1025',
+                'host' => 'mail'
+            ],
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => false,
+        ],
+    ],
+];
